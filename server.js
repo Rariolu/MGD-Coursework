d15655c646d8384018122ce227959251fa90ca3d
Rariolu <rcoyneyr82011@gmail.com>
2020-11-26 00:25:08 +0000

    "MainGame" and "Gameover" pages added with SocketIO connection

--- server.js
+++ server.js
@@ -5,9 +5,37 @@ var io = require("socket.io")(http);
 var introScreen = function(req, res)
 {
     res.sendFile(__dirname+"/intro.html");
-}
+};
 
 app.get("/",introScreen);
+
+var mainGame = function(req, res)
+{
+    res.sendFile(__dirname+"/maingame.html");
+};
+
+app.get("/maingame", mainGame);
+
+var gameover = function(req, res)
+{
+    res.sendFile(__dirname+"/gameover.html");
+};
+
+app.get("/gameover",gameover);
+
+var ioConnection = function(socket)
+{
+    console.log("Client connected.");
+    
+    var ioDisconnection = function()
+    {
+        console.log("Client disconnected.");
+        
+    };
+    socket.on("disconnect",ioDisconnection);
+};
+
+io.on("connection",ioConnection);
 
 var main = function()
 {
