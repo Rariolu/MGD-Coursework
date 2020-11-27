8123fe6969ffa4224322761253193f3625d27245
Rariolu <rcoyneyr82011@gmail.com>
2020-11-27 14:03:57 +0000

    Remove redundant functionality

--- client.js
+++ client.js
@@ -1,29 +1,8 @@
 var socket;
-var gamePage;
 
 function Initialisation()
 {
-    gamePage = document.getElementById("gamepage");
     
     socket = io();
-    socket.on("pageContents",LoadPageContents);
     
-    socket.on("nickname", Nickname);
-}
-
-function Nickname(nickname)
-{
-    document.getElementById("beep").innerHTML = nickname;
-}
-
-function LoadPageContents(page)
-{
-    gamePage.innerHTML = page;
-}
-
-function btnSubmit_Click()
-{
-    var nickname = document.getElementById("tbNickname").value;
-
-    socket.emit("startgame",nickname);
 }
