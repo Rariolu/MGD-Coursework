851ef2f994b15d05b90f6763c5af5a9e5abec395
Rariolu <rcoyneyr82011@gmail.com>
2020-12-06 20:11:38 +0000

    Created event handler that handles "playerdied" event

--- client.js
+++ client.js
@@ -155,6 +155,12 @@ function Initialisation()
     socket.on("coindespawn", CoinDelete);
     socket.on("scorechanged", ScoreChanged);
     socket.on("playershot", PlayerShot);
+    socket.on("playerdied", PlayerDied);
+}
+
+function PlayerDied(playerID)
+{
+    PlayerDespawn(playerID);
 }
 
 function PlayerShot(playerID, lives)
